914c82b63c246bed1ec9dfefd83cd1ac1d274b6d
EC2 Default User <ec2-user@ip-172-31-14-137.ap-northeast-1.compute.internal>
2024-01-10 10:14:03 +0000

    Add controllers

---
+++ app/controllers/public/post_images_controller.rb
@@ -0,0 +1,10 @@
+class Public::PostImagesController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,42 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'searches/search'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'searches/search'
+  end
+  namespace :public do
+    get 'relationships/followings'
+    get 'relationships/followers'
+  end
+  namespace :public do
+    get 'favorites/index'
+  end
+  namespace :public do
+    get 'post_images/new'
+    get 'post_images/show'
+    get 'post_images/index'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/check'
+  end
+  namespace :public do
+    get 'homes/top'
+  end
   # 顧客用
   # URL /users/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
