ed9962bcb8d323ceda44842003d5ab02f62ba2da
EC2 Default User <ec2-user@ip-172-31-14-137.ap-northeast-1.compute.internal>
2024-01-10 12:10:26 +0000

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'searches/search'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'searches/search'
-  end
-  namespace :public do
-    get 'relationships/followings'
-    get 'relationships/followers'
-  end
-  namespace :public do
-    get 'favorites/index'
-  end
-  namespace :public do
-    get 'post_images/new'
-    get 'post_images/show'
-    get 'post_images/index'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/check'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
+  
   # 顧客用
   # URL /users/sign_in ...
   devise_for :users, skip: [:passwords], controllers: {
@@ -48,5 +11,31 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  scope module: :public do
+    root to: "homes#top"
+    resources :post_images, only: [:new, :create, :show, :index, :destroy] do
+      resource :favorite, only: [:create, :destroy, :index]
+      resources :comments, only: [:create, :destroy]
+    end
+    
+    get "search" => "searches#search"
+    get "users/check" => "users#check"
+    patch "users/withdraw" => "users#withdraw"
+    
+    resources :users, only: [:show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+  end
+  
+  namespace :admin do
+    resources :genres, only: [:create, :index, :edit, :update, :destroy]
+    resources :users, only: [:show, :index, :edit, :update]
+    
+    root to: "homes#top"
+    get "search" => "searches#search"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
