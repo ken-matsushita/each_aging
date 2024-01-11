a265e805180d81bd5622b9574202f0821fbb572a
EC2 Default User <ec2-user@ip-172-31-14-137.ap-northeast-1.compute.internal>
2024-01-11 11:04:40 +0000

    Add user_function

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,12 +1,44 @@
 class Public::UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
   
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
   end
 
   def edit
+    @user = User.find(params[:id])
   end
-
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "successfully"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
   def check
   end
+  
+  def withdraw
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :account_name, :profile_image, :introduction, :is_active)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      user = current_user
+      redirect_to user_path(user.id)
+    end
+  end
+  
 end
