5081545df73d671066ef6f9830fe14e55cb1e3f6
EC2 Default User <ec2-user@ip-172-31-14-137.ap-northeast-1.compute.internal>
2024-01-13 06:52:17 +0000

    Add postimage_new

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -2,6 +2,19 @@ class Public::PostImagesController < ApplicationController
   before_action :authenticate_user!
   
   def new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    if @post_image.save
+      flash[:notice] = "投稿に成功しました。"
+      redirect_to user_path(current_user)
+    else
+      flash[:alert] = "投稿に失敗しました。"
+      render :new
+    end
   end
 
   def show
@@ -9,4 +22,11 @@ class Public::PostImagesController < ApplicationController
 
   def index
   end
+  
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:user_id, :genre_id, :name, :care, :years_passed, :body, :image)
+  end
+  
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,5 +1,10 @@
 class PostImage < ApplicationRecord
+  belongs_to :user
   belongs_to :genre
+  has_many :comments
+  has_many :favorites
+  
+  has_one_attached :image
   
   validates :image, presence: true
 end
