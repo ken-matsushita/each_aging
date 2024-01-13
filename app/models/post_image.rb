8aaaec35eec79bc41738883487d1f5f1021186fa
EC2 Default User <ec2-user@ip-172-31-14-137.ap-northeast-1.compute.internal>
2024-01-13 15:10:15 +0000

    Fixed postimage

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -7,6 +7,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   
   validates :image, presence: true
+  validates :genre_id, presence: true
   
   def get_image(width, height)
     image.variant(resize_to_limit: [width, height]).processed
