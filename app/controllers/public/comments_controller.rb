1c463988bd18fdcb78c8b70b741665512a88e254
EC2 Default User <ec2-user@ip-172-31-14-137.ap-northeast-1.compute.internal>
2024-01-20 05:25:40 +0000

    Fixed asynchronous communication

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,15 +1,16 @@
 class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
-    post_image = PostImage.find(params[:post_image_id])
+    @post_image = PostImage.find(params[:post_image_id])
     comment = current_user.comments.new(comment_params)
-    comment.post_image_id = post_image.id
+    comment.post_image_id = @post_image.id
     comment.save
   end
-  
+
   def destroy
     Comment.find(params[:id]).destroy
+    @post_image = PostImage.find(params[:post_image_id])
   end
 
   private
